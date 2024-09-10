0a99fc1199e6b9c24cfe96cfed78cbe2352e7a87
divyasree1914 <151026946+divyasree1914@users.noreply.github.com>
2024-09-10 21:36:56 +0530

    added mongo url in .env

--- server/mongo_connection/connection.js
+++ server/mongo_connection/connection.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+dotenv.config()
+const mongo_uri = process.env.mongo_uri
 const Connection = async ()=>{
     try{
-        const result = await mongoose.connect("mongodb://localhost:27017/alpha")
+        const result = await mongoose.connect(`${mongo_uri}`)
         console.log("connected to mongodb")
     }catch{
         console.log("error in connectiong to mongo db")
